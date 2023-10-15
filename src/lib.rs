1c8b2af7ff9ae35098fd26413fa11c8ba5742f52
airidaceae <78971823+airidaceae@users.noreply.github.com>
2023-10-15 01:24:24 -0400

    Update licensing information

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2023 Marceline Cramer
+// Copyright (c) 2023 Roux
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // This file is part of Flue.
--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,22 @@
+// Copyright (c) 2023 Marceline Cramer
+// Copyright (c) 2023 Roux
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This file is part of Flue.
+//
+// Flue is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Flue is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Flue. If not, see <https://www.gnu.org/licenses/>.
+
 use super::*;
 
 #[tokio::test]
